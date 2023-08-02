9eba396175ad9a211879a923a7f5aaf08ddd6d19
Agchetz <agchetz@gmail.com>
2023-08-02 11:13:30 +0530

    cors change backend

--- backend/routes/employee.route.js
+++ backend/routes/employee.route.js
@@ -3,21 +3,6 @@ const app = express();
 const employeeRoute = express.Router();
 const cors = require("cors");
 
-// CORS OPTIONS
-var whitelist = ["http://localhost:4200", "http://localhost:4000"];
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = {
-      origin: "*",
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    };
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions);
-};
-
 // Employee model
 let Employee = require("../models/Employee");
 
