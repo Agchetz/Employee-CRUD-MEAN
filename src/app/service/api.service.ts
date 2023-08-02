c1c0859b5846b43a6bc80a9e927359f24d678aa6
Chethan A <86460271+Agchetz@users.noreply.github.com>
2023-08-02 19:16:37 +0530

    Update api.service.ts

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -11,7 +11,7 @@ import {
   providedIn: 'root',
 })
 export class ApiService {
-  baseUri: string = 'http://15.207.51.50:4000/api';
+  baseUri: string = 'https://13.126.190.210:4000/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(private http: HttpClient) {}
